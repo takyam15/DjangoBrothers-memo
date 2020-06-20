a5924daefdc61227c89df8211645c026b6c66f9e
takyam15 <u779052i@alumni.osaka-u.ac.jp>
2020-06-20 22:09:12 +0900

    Updated layouts in the template

--- memo/forms.py
+++ memo/forms.py
@@ -1,9 +1,31 @@
-from django.forms import ModelForm
+from django import forms
 
 from .models import Memo
 
 
-class MemoForm(ModelForm):
+class MemoForm(forms.ModelForm):
+    title = forms.CharField(
+        label='タイトル',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'タイトル'
+        }),
+    )
+    slug = forms.CharField(
+        label='スラッグ',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'スラッグ'
+        })
+    )
+    text = forms.CharField(
+        label='本文',
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': '本文'
+        })
+    )
+
     class Meta:
         model = Memo
-        fields = ['title', 'slug', 'text']
+        fields = ('title', 'slug', 'text')
